88a4e5005cdb85ec97b6e617fe4858c42609a6d2
Daniel Reguero <daniel@altr.com>
2021-04-12 11:33:27 -0400

    log error and exist, update loggers a bit

--- client.js
+++ client.js
@@ -18,16 +18,27 @@ function main() {
   const client = new hello_proto.Greeter("0.0.0.0:50051", grpc.credentials.createInsecure());
   const toSend = {name: 'test'};
   const meta = new grpc.Metadata();
+  meta.set("custom", "some-value");
   // what's the correct way to get the content-length?
+
+  const contentLength = Buffer.from(JSON.stringify(toSend)).byteLength;
+  console.log(contentLength);
+
+  meta.set("content-length", contentLength); // some library; result: 16 - fails
 
   // meta.set("content-length", sizeof(toSend)); // some library; result: 16 - fails
   // meta.set('content-length', Buffer.from(JSON.stringify(toSend)).byteLength); // result: 15 - fails
   // meta.set('content-length', client.SayHello.requestSerialize(toSend).length); // result: 6 - fails
-  // meta.set('content-length', client.SayHello.requestSerialize(toSend).length + 5); // result: 11 (serialize + 5 ) - success
-  console.log(meta);
+  // meta.set('content-length', client.SayHello.requestSerialize(toSend).length + 5); // result: 11 (serialize + 5) - success
+  
+
+  // console.log(meta);
   client.sayHello(toSend, meta, function(err, response) {
-    console.log(err);
-    console.log('Greeting:', response);
+    if(err) {
+      console.log(err);
+      return;
+    }
+    console.log('Greetings:', response.message);
   });
 }
 
